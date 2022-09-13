5702bbd7b2206c6571a5998ac21abcb84efd9123
Ariff <ariffazlan16@gmail.com>
2022-09-13 09:24:13 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { render } from "react-dom";
+import MyComponent from "./MyComponent";
+import Clock from "./Clock";
+import NameForm from "./NameForm";
+import Game from "./Game";
+
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  );
+}
+export default App;
---
+++ src/Testing.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class MyComponent extends Component {
+  state = {
+    first: false,
+    second: true
+}
+}
